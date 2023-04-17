836244f9d792890705305c74fac75255424b6efb
KimJM <iukim21c@gmail.com>
2023-04-18 01:05:43 +0900

    구름 오르기 게임

--- Assets/game2/CatPlayerController.cs
+++ Assets/game2/CatPlayerController.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CatPlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
+    Animator animator;
     float jumpForce = 680.0f;
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
@@ -12,14 +14,16 @@ public class CatPlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.rigid2D = GetComponent<Rigidbody2D>();    
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        if(Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0) {
 
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -36,5 +40,25 @@ public class CatPlayerController : MonoBehaviour
             transform.localScale = new Vector3(key, 1, 1);
         }
 
+        // ¼Óµµ¿¡ µû¶ó ¸ÂÃá´Ù.
+        if(this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        } else
+        {
+            this.animator.speed = 1.0f;
+        }
+        
+
+        if (transform.position.y < -10)
+        {
+            SceneManager.LoadScene("game2/ClimbCloud");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("°ñ");
+        SceneManager.LoadScene("game2/ClearScene");
     }
 }
